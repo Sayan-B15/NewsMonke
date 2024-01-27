b3ab5f93a043a8b0d29c9e9854096e1e8c7d6227
Sayan-B15 <109094826+Sayan-B15@users.noreply.github.com>
2024-01-27 16:22:02 +0530

    comit1

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
-import Newsitem from './Newsitem'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import Newsitem from './Newsitem';
+import PropTypes from 'prop-types';
 
 export class News extends Component {
 
@@ -14,7 +14,6 @@ export class News extends Component {
     country: PropTypes.string,
     pageSize: PropTypes.number,
     category: PropTypes.string,
-
   }
 
   constructor(props) {
@@ -22,62 +21,44 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
-      page: 1
-    }
+      page: 1,
+      totalArticles: 0
+    };
     document.title = `${this.props.category} - NewsMonkey`;
-
   }
 
+  async updateNews() {
+    const url = `https://gnews.io/api/v4/top-headlines?country=${this.props.country}&topic=${this.props.category}&token=b6070d187a661e6e71b9a28bd68fff84&max=${this.props.pageSize}&page=${this.state.page}`;
 
-  async updateNews(){
-    
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d0d40ab97ce94efb953e4129c26f25ff&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    let data = await fetch(url);
-    let parsedData = await data.json()
-    console.log(parsedData);
-    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
+    try {
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({ articles: parsedData.articles, totalArticles: parsedData.totalArticles });
+    } catch (error) {
+      console.error('Error fetching news:', error);
+    }
   }
 
   async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d0d40ab97ce94efb953e4129c26f25ff&page=1&pageSize=${this.props.pageSize}`;
-    let data = await fetch(url);
-    let parsedData = await data.json()
-    console.log(parsedData);
-    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
+    this.updateNews();
   }
 
-
-
   handlePrevClick = async () => {
-    //console.log("Previous");
-   // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d0d40ab97ce94efb953e4129c26f25ff&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-    //let data = await fetch(url);
-   // let parsedData = await data.json()
-    //console.log(parsedData);
-    //this.setState({
-     // page: this.state.page - 1,
-     // articles: parsedData.articles
-    //})
-    this.setState({page: this.state.page - 1});
-     this.updateNews();
+    if (this.state.page > 1) {
+      this.setState({ page: this.state.page - 1 }, () => {
+        this.updateNews();
+      });
+    }
   }
 
   handleNextClick = async () => {
-    console.log("Next");
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
-
-    }
-    else {
-
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d0d40ab97ce94efb953e4129c26f25ff&page=${this.state.page + 1}pageSize=${this.props.pageSize}`;
-
-      let data = await fetch(url);
-      let parsedData = await data.json()
-      console.log(parsedData);
-      this.setState({
-        page: this.state.page + 1,
-        articles: parsedData.articles
-      })
+    const { page, totalArticles } = this.state;
+    const totalPages = Math.ceil(totalArticles / this.props.pageSize);
+    if (page < totalPages) {
+      this.setState({ page: page + 1 }, () => {
+        this.updateNews();
+      });
     }
   }
 
@@ -87,22 +68,27 @@ export class News extends Component {
         <h1 className="text-center" style={{ margin: '35px 0px' }}>NewsMonkey - One Stop destination for all types of news</h1>
         <div className="row">
           {this.state.articles.map((element) => {
-            return <div className="col-md-4" key={element.url}>
-              <Newsitem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} auhtor={element.author} date={element.publishedAt} />
-            </div>
+            return (
+              <div className="col-md-4" key={element.url}>
+                <Newsitem
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imageUrl={element.image}
+                  newsUrl={element.url}
+                  author={element.source.name}
+                  date={element.publishedAt}
+                />
+              </div>
+            );
           })}
-
         </div>
-        <div className="conatiner d-flex justify-content-between">
+        <div className="container d-flex justify-content-between">
           <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
-
-          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 20)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
-
+          <button disabled={this.state.page * this.props.pageSize >= this.state.totalArticles} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
-
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
